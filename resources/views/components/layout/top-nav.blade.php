d64a8c48e4cfd4eb709a42f16a626010ac6a9e60
Pascal Schreiber <pascal.schreiber.94@gmx.de>
2021-11-01 23:13:40 +0100

    Fixed Title link

--- resources/views/components/layout/top-nav.blade.php
+++ resources/views/components/layout/top-nav.blade.php
@@ -1,4 +1,4 @@
-    <a class="navbar-brand ps-3" href="index.php">Bundesliga Tippspiel</a>
+    <a class="navbar-brand ps-3" href="{{ route('home') }}">Bundesliga Tippspiel</a>
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
     <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
